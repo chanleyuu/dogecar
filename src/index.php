15cfc997abb07241d6f5c88b5c8b2c05810df874
chanleyuu <hanley-c3@ulster.ac.uk>
2021-03-21 22:32:39 +0000

    modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,10 @@
 <?php
 
-    echo "<p>Welcome to DogeCar</p>"
+   // echo "<h1>Welcome to DogeCar</h1>"
+    
 ?>
+<html>
+<head>DogeCar></head>
+<form action = "<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+<input type="text" name="search" />
+<p><input type="submit" value="Convert Amount"/></p>
