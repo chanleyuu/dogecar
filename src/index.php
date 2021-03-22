13381472fee9359fe1605b515c58ca6e1ff2098a
chanleyuu <56023204+chanleyuu@users.noreply.github.com>
2021-03-22 10:08:19 +0000

    modified:   src/index.php 	new file:   src/search.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,11 @@
 <?php
 
    // echo "<h1>Welcome to DogeCar</h1>"
-    
+    include("search.php");
+	
 ?>
 <html>
 <head>DogeCar></head>
 <form action = "<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
 <input type="text" name="search" />
-<p><input type="submit" value="Convert Amount"/></p>
+<p><input type="submit" value="Search"/></p>
