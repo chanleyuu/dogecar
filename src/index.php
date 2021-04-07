a7483731866e76186d971f9a0a8d3311fd54c327
chanleyuu <hanley-c3@ulster.ac.uk>
2021-04-07 01:05:45 +0100

    added a search icon

--- src/index.php
+++ src/index.php
@@ -21,5 +21,5 @@
 <img src="../res/dogehead.png" alt="HTML5 Icon" style="width:128px;height:128px;">
 <form action = "<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
 <input type="text" name="search" />
-<p><input type="submit" value="Search"/></p>
+<p><input type="submit" value="Search"/><img src="../res/search.png" alt="HTML5 Icon" style="width:128px;height:128px;"></p>
 </body>
