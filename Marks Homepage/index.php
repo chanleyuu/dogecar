ace7c9d6ca489fa43889d0e171dcdf896120b72a
Mark McCormick <McCormick-M15@ulster.ac.uk>
2021-04-19 12:29:35 +0100

    Uploaded unfinished homepage

---
+++ Marks Homepage/index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/Golf.png" class="imgLeft" />
+<h3>Wow</h3>
+<p>
+  New and Used Stock
+</p>
+
+<img src="Images/Engine.png" class="imgLeft"/>
+<h3>Wow</h3>
+<p>
+Genuine Dealership Parts
+
+</p>
+
+<img src="Images/Salesman.png" class="imgLeft" />
+<h3>Wow</h3>
+<p>
+First Class Service
+</p>';
+
+include 'Template.php';
+?>
