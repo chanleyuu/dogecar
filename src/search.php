466dba002ef43bfce5822ace795ad495b9b70862
chanleyuu <hanley-c3@ulster.ac.uk>
2021-05-01 17:15:54 +0100

    Getting searchpage ready

--- src/search.php
+++ src/search.php
@@ -77,8 +77,9 @@ class Search {
         //$sql->bind_param('s', $Search);
         $result = $conn->query($sql, MYSQLI_STORE_RESULT);
         */
-        $stmt = $pdo->prepare("SELECT id, name FROM products WHERE name LIKE '%?%' ");
-        $stmt->execute(array('?' => $Search));
+        $stmt = $pdo->prepare("SELECT id, name FROM products WHERE name LIKE '%".$Search."%' ");
+       // $stmt->execute(array('?' => $Search));
+        $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         /*
         if ($result->num_rows > 0) {
